e1ddbfa414bffbde11beaf42c9beb829ce8ab487
Muhammad <talha18513@gmail.com>
2024-10-10 07:05:15 +0500

    I added text to the file

---
+++ Docs/javascript/footer.js
@@ -0,0 +1,96 @@
+document.getElementById("footer").innerHTML = `
+    <div class="bg-[#f8931f] p-5">
+        <div
+            class="max-w-7xl w-full mx-auto flex items-center xl:justify-center xl:flex-nowrap gap-x-20 sm:gap-y-10 gap-y-5 flex-wrap">
+            <div class="">
+                <p class="font-semibold">Address</p>
+                <p class=" md:text-2xl text-lg text-zinc-800">Schillerstr. 25, 12529 Schönfeld bei Berlin</p>
+            </div>
+            <div class="">
+                <p class="font-semibold">Telefon</p>
+                <p class=" md:text-2xl text-lg text-zinc-800">03379 / 582 82 82</p>
+            </div>
+            <div class="">
+                <p class="font-semibold">Telefax</p>
+                <p class=" md:text-2xl text-lg text-zinc-800">03379 / 582 82 83</p>
+            </div>
+            <div class="">
+                <p class="font-semibold">E-Mail</p>
+                <p class=" md:text-2xl text-lg text-zinc-800">buero@lorenzkg.de</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="">
+        <iframe class="w-full md:h-[25rem] h-[20rem]"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14159999.604600862!2d58.34639765869034!3d29.949782516630794!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38db52d2f8fd751f%3A0x46b7a1f7e614925c!2sPakistan!5e0!3m2!1sen!2s!4v1728449406296!5m2!1sen!2s"
+            style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+
+    <div class="bg-[#7c6498]">
+        <p class="max-w-7xl w-full mx-auto text-white p-2">
+            © 2024 Lorenz Malermeister Fassadensanierungs- GmbH | Webdesign: <a href="" class="hover:underline">Zahn
+                Internet Consult</a>
+        </p>
+    </div>
+
+
+    <div class="bg-[#f8931f] p-5 ">
+
+        <div class="max-w-7xl w-full mx-auto sm:py-12 py-5">
+
+            <div
+                class="grid mx-auto lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-1 text-[15px] sm:gap-10 gap-6">
+
+                <div class="w-full sm:col-span-2">
+                    <h2 class="uppercase font-semibold  text-[#333333]">Fassadensanierungen in Berlin Brandenburg</h2>
+                    <p class="sm:my-4 my-2 max-w-[370px] w-full">Gegründet im Oktober 1974 in Berlin-Tempelhof durch
+                        Rainer Lorenz. Es gelang im
+                        Laufe der Jahre Hausverwaltungen als Stammkunden zu gewinnen, für welche die Lorenz Malermeister
+                        GmbH inzwischen teilweise über 20 Jahre arbeitet.</p>
+                    <div class="max-w-[360px] w-full">
+                        <img src="/Docs/Assets/Images/footer/img1.webp" class=" w-full" alt="">
+                    </div>
+                </div>
+
+                <div class="">
+                    <h2 class="uppercase font-semibold  text-[#333333]">Menü</h2>
+
+                    <ul class="sm:mt-4 mt-2 space-y-2">
+                        <li><a href="#" class="hover:underline">Startseite</a></li>
+                        <li><a href="#" class="hover:underline">Über uns</a></li>
+                        <li><a href="#" class="hover:underline">Referenzen</a></li>
+                        <li><a href="#" class="hover:underline">Service</a></li>
+                        <li><a href="#" class="hover:underline">Ausbildung</a></li>
+                    </ul>
+                </div>
+                <div class="">
+                    <h2 class="uppercase font-semibold  text-[#333333]">Leistungen
+                    </h2>
+
+                    <ul class="sm:mt-4 mt-2 space-y-2">
+                        <li><a href="#" class="hover:underline">Fassadensanierung</a></li>
+                        <li><a href="#" class="hover:underline">Betonsanierung</a></li>
+                        <li><a href="#" class="hover:underline">Balkonsanierung</a></li>
+                        <li><a href="#" class="hover:underline">Maler- & Tapezierarbeiten</a></li>
+                        <li><a href="#" class="hover:underline">Treppenhaussanierung</a></li>
+                        <li><a href="#" class="hover:underline">Putzsanierung</a></li>
+                        <li><a href="#" class="hover:underline">Stucksanierung</a></li>
+                    </ul>
+                </div>
+                <div class="">
+                    <h2 class="uppercase font-semibold  text-[#333333]">Meta</h2>
+
+                    <ul class="sm:mt-4 mt-2 space-y-2">
+                        <li><a href="#" class="hover:underline">Impressum</a></li>
+                        <li><a href="#" class="hover:underline">Datenschutz</a></li>
+                    </ul>
+                </div>
+
+            </div>
+
+
+        </div>
+
+    </div>
+`
---
+++ Docs/javascript/navbar.js
@@ -0,0 +1,245 @@
+
+document.getElementById("navbar").innerHTML = `
+    <div class="fixed bottom-28 right-0 overflow-hidden flex flex-col gap-3 z-40">
+        <a href="">
+            <div
+                class="flex border  items-center translate-x-[77%] hover:translate-x-[0%] transition-all duration-500 gap-4 py-2 px-3 bg-[#f8931f] hover:bg-[#7c6498] rounded-l-md text-white">
+                <i class="fa-solid fa-phone text-3xl py-[5px]"></i>
+                <p class="text-lg font-semibold pl-1 "> 03379 / 582 82 82</p>
+            </div>
+        </a>
+        <a href="">
+            <div
+                class="flex border items-center translate-x-[77%] hover:translate-x-[0%] transition-all duration-500 gap-4 py-2 px-3 bg-[#f8931f] hover:bg-[#7c6498] rounded-l-md text-white">
+                <i class="fa-solid fa-envelope text-4xl "></i>
+                <div class="">
+                    <p class="text-lg font-bold max-w-[12rem] leading-tight">E-Mail schreiben <span
+                            class="font-medium text-nowrap">An: buero@lorenzkg.de</span></p>
+                </div>
+            </div>
+        </a>
+    </div>
+
+    <div id="scrollTopButton"
+        class="fixed hidden bg-[#7c6498] fastFadeIn right-5 bottom-5 cursor-pointer transition-all duration-500 hover:bg-[#674989] w-14 h-14 rounded-full flex items-center justify-center">
+        <i class="fa-solid fa-chevron-up text-white text-2xl"></i>
+    </div>
+
+    <p class="bg-[#f2f2f2] text-zinc-500 text-center py-2 px-4 text-sm ">Lorenz Malermeister
+        Fassadensanierungs- GmbH &nbsp; • &nbsp; Schillerstr. 25, 12529 Schönefeld bei Berlin &nbsp; • &nbsp; Telefon:
+        <a class="hover:underline hover:text-black transition-all duration-500" href="">03379 / 582 82 82</a> &nbsp; •
+        &nbsp; Mail:
+        <a class="hover:underline hover:text-black transition-all duration-500"
+            href="mailto:buero@lorenzkg.de">buero@lorenzkg.de</a>
+    </p>
+
+    <div class="md:px-10 px-5 py-8">
+        <div class="max-w-7xl w-full mx-auto">
+            <div class="flex items-center justify-between gap-3">
+                <a href="/index.html" class="flex md:items-center md:gap-5 gap-3 md:flex-row flex-col">
+                    <img src="/Docs/Assets/Images/Home/logo.png" class="md:max-w-[285px] max-w-[200px] w-full" alt="">
+                    <p class="font-semibold text-[#7f565a] max-w-[15rem] leading-tight md:leading-normal w-full">
+                        MALERMEISTER & TECHNIKER SEIT ÜBER 40
+                        JAHREN</p>
+                </a>
+
+                <ul class="xl:flex hidden items-center gap-5 font-semibold text-lg text-[#f8931f]">
+                    <li><a href="#" class="hover:text-[#7f565a] transition-all py-3 duration-500 block">ÜBER UNS</a>
+                    </li>
+                    <li><a href="#" class="hover:text-[#7f565a] transition-all py-3 duration-500 block">REFERENZEN</a>
+                    </li>
+                    <li><a href="#" class="hover:text-[#7f565a] transition-all py-3 duration-500 block">SERVICE</a></li>
+                    <li><a href="#" class="hover:text-[#7f565a] transition-all py-3 duration-500 block">AUSBILDUNG</a>
+                    </li>
+                    <li><a href="#" class="hover:text-[#7f565a] transition-all py-3 duration-500 block">GALERIE</a></li>
+                    <li><a href="#" class="hover:text-[#7f565a] transition-all py-3 duration-500 block">KONTAKT</a></li>
+                </ul>
+
+                <div class="flex xl:hidden items-center justify-center cursor-pointer gap-3 text-white bg-[#7c6498] py-1 px-4 rounded-sm"
+                    id="openNav2">
+                    <i class="fa-solid fa-bars"></i> MENU
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="md:px-10 px-5  bg-[#7c6498] sticky top-0 z-50 border-b">
+        <div class="max-w-7xl w-full mx-auto flex items-center justify-between transition-all duration-500">
+            <img src="/Docs/Assets/Images/Home/whiteLogo.png"
+                class="max-w-[150px] xl:hidden w-full fastFadeIn py-4 transition-all duration-500" id="whiteLogo"
+                alt="">
+            <ul class="xl:flex hidden items-center gap-10 font-semibold text-[#ffffff] transition-all duration-500">
+                <li id="dropDown" class=" relative cursor-pointer"><a
+                        class="hover:text-[#f8931f] transition-all py-5 duration-500 block"
+                        id="arrowRotate">FASSADENSANIERUNG <i
+                            class="fa-solid fa-angle-down ml-1 transition-transform duration-500"></i></a>
+                    <ul
+                        class="absolute fastFadeIn bg-[#8670A0] transition-all duration-500 rounded-b-md overflow-hidden">
+                        <li><a href="#"
+                                class=" transition-all p-4 px-5 hover:bg-[#63517b] border-b border-white duration-500 block">Beschichtungssysteme</a>
+                        </li>
+                        <li><a href="#"
+                                class=" transition-all p-4 px-5 hover:bg-[#63517b] border-b border-white duration-500 block">Rissarmierung</a>
+                        </li>
+                        <li><a href="#"
+                                class=" transition-all p-4 px-5 text-nowrap hover:bg-[#63517b] border-b border-white duration-500 block">Putzarbeiten
+                                / Rissursachen</a>
+                        </li>
+                        <li><a href="#"
+                                class=" transition-all p-4 px-5 hover:bg-[#63517b] border-b border-white duration-500 block">Prüfmethoden</a>
+                        </li>
+                        <li><a href="#"
+                                class=" transition-all p-4 px-5 hover:bg-[#63517b] border-b border-white duration-500 block">Algenbekämpfung</a>
+                        </li>
+                        <li><a href="#"
+                                class=" transition-all p-4 px-5 hover:bg-[#63517b] border-b border-white duration-500 block">Graffitibekämpfung</a>
+                        </li>
+                    </ul>
+                </li>
+                <li id="dropDown" class=" relative cursor-pointer"><a
+                        class="hover:text-[#f8931f] transition-all py-5 duration-500 block"
+                        id="arrowRotate">BETONSANIERUNG<i
+                            class="fa-solid fa-angle-down ml-1 transition-transform duration-500"></i></a>
+                    <ul
+                        class="absolute fastFadeIn bg-[#8670A0] transition-all duration-500 rounded-b-md overflow-hidden">
+                        <li><a href="#"
+                                class=" transition-all p-4 px-5 hover:bg-[#63517b] border-b border-white duration-500 block">Arbeitsschritte</a>
+                        </li>
+                        <li><a href="#"
+                                class=" transition-all text-nowrap p-4 px-5 hover:bg-[#63517b] border-b border-white duration-500 block">Detailinformationen
+                                zur Betonsanierung</a>
+                        </li>
+                    </ul>
+                </li>
+                <li id="dropDown" class=" relative cursor-pointer"><a
+                        class="hover:text-[#f8931f] transition-all py-5 duration-500 block"
+                        id="arrowRotate">BALKONSANIERUNG <i
+                            class="fa-solid fa-angle-down ml-1 transition-transform duration-500"></i></a>
+                    <ul
+                        class="absolute fastFadeIn bg-[#8670A0] transition-all duration-500 rounded-b-md overflow-hidden">
+                        <li><a href="#"
+                                class=" transition-all p-4 px-5 hover:bg-[#63517b] border-b border-white duration-500 block">Balkonabdichtung</a>
+                        </li>
+                        <li><a href="#"
+                                class=" transition-all text-nowrap p-4 px-5 hover:bg-[#63517b] border-b border-white duration-500 block">Detailinformationen
+                                zu Balkonabdichtungen</a>
+                        </li>
+                        <li><a href="#"
+                                class=" transition-all p-4 px-5 text-nowrap hover:bg-[#63517b] border-b border-white duration-500 block">Beschreibung
+                                der Problemfelder...</a>
+                        </li>
+                    </ul>
+                </li>
+                <li><a href="#" class="hover:text-[#f8931f] transition-all py-5 duration-500 block">PUTZSANIERUNG</a>
+                </li>
+                <li><a href="#" class="hover:text-[#f8931f] transition-all py-5 duration-500 block">STUCKSANIERUNG</a>
+                </li>
+            </ul>
+            <i class="fa-solid fa-bars text-white text-2xl cursor-pointer xl:hidden" id="openNav"></i>
+        </div>
+    </div>
+
+    <div class="bg-[#7c6498] transition-all duration-500 fixed top-0 right-0 z-50  overflow-hidden"
+        id="responsiveNavbar" style="width: 0rem;">
+        <ul class="flex flex-col font-semibold h-screen p-5 text-[#ffffff] mt-10 relative">
+            <i class="fa-solid fa-x text-2xl text-white absolute -top-7 cursor-pointer p-1 left-4 " id="closeNav"></i>
+            <li id="resDropDown" class="relative cursor-pointer"><a
+                    class="hover:text-[#f8931f] transition-all py-5 duration-500 justify-between flex items-center"
+                    id="arrowRotate">FASSADENSANIERUNG <i
+                        class="fa-solid fa-angle-down ml-1 transition-transform duration-500"></i></a>
+                <ul class=" bg-[#8670A0] fastFadeIn transition-all duration-500 rounded-b-md overflow-hidden">
+                    <li><a href="#"
+                            class=" transition-all p-4 px-5 hover:bg-[#63517b] border-b border-white duration-500 block">Beschichtungssysteme</a>
+                    </li>
+                    <li><a href="#"
+                            class=" transition-all p-4 px-5 hover:bg-[#63517b] border-b border-white duration-500 block">Rissarmierung</a>
+                    </li>
+                    <li><a href="#"
+                            class=" transition-all p-4 px-5 text-nowrap hover:bg-[#63517b] border-b border-white duration-500 block">Putzarbeiten
+                            / Rissursachen</a>
+                    </li>
+                    <li><a href="#"
+                            class=" transition-all p-4 px-5 hover:bg-[#63517b] border-b border-white duration-500 block">Prüfmethoden</a>
+                    </li>
+                    <li><a href="#"
+                            class=" transition-all p-4 px-5 hover:bg-[#63517b] border-b border-white duration-500 block">Algenbekämpfung</a>
+                    </li>
+                    <li><a href="#"
+                            class=" transition-all p-4 px-5 hover:bg-[#63517b] border-b border-white duration-500 block">Graffitibekämpfung</a>
+                    </li>
+                </ul>
+            </li>
+            <li id="resDropDown" class="relative cursor-pointer"><a
+                    class="hover:text-[#f8931f] transition-all py-5 duration-500 justify-between flex items-center"
+                    id="arrowRotate">BETONSANIERUNG<i
+                        class="fa-solid fa-angle-down ml-1 transition-transform duration-500"></i></a>
+                <ul class=" bg-[#8670A0] fastFadeIn transition-all duration-500 rounded-b-md overflow-hidden">
+                    <li><a href="#"
+                            class=" transition-all p-4 px-5 hover:bg-[#63517b] border-b border-white duration-500 block">Arbeitsschritte</a>
+                    </li>
+                    <li><a href="#"
+                            class=" transition-all p-4 px-5 hover:bg-[#63517b] border-b border-white duration-500 block">Detailinformationen
+                            zur Betonsanierung</a>
+                    </li>
+                </ul>
+            </li>
+            <li id="resDropDown" class="relative cursor-pointer"><a
+                    class="hover:text-[#f8931f] transition-all py-5 duration-500 justify-between flex items-center"
+                    id="arrowRotate">BALKONSANIERUNG
+                    <i class="fa-solid fa-angle-down ml-1 transition-transform duration-500"></i></a>
+                <ul class=" bg-[#8670A0] fastFadeIn transition-all duration-500 rounded-b-md overflow-hidden">
+                    <li><a href="#"
+                            class=" transition-all p-4 px-5 hover:bg-[#63517b] border-b border-white duration-500 block">Balkonabdichtung</a>
+                    </li>
+                    <li><a href="#"
+                            class=" transition-all  p-4 px-5 hover:bg-[#63517b] border-b border-white duration-500 block">Detailinformationen
+                            zu Balkonabdichtungen</a>
+                    </li>
+                    <li><a href="#"
+                            class=" transition-all p-4 px-5  hover:bg-[#63517b] border-b border-white duration-500 block">Beschreibung
+                            der Problemfelder...</a>
+                    </li>
+                </ul>
+            </li>
+            <li><a href="#" class="hover:text-[#f8931f] transition-all py-5 duration-500 block">PUTZSANIERUNG</a>
+            </li>
+            <li><a href="#" class="hover:text-[#f8931f] transition-all py-5 duration-500 block">STUCKSANIERUNG</a>
+            </li>
+        </ul>
+    </div>
+`
+
+const scrollTopButton = document.getElementById('scrollTopButton');
+const whiteLogo = document.getElementById("whiteLogo")
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 160 || document.documentElement.scrollTop > 160) {
+        scrollTopButton.classList.remove('hidden');
+        whiteLogo.classList.remove('xl:hidden');
+    } else {
+        whiteLogo.classList.add('xl:hidden');
+        scrollTopButton.classList.add('hidden');
+    }
+};
+
+scrollTopButton.addEventListener('click', function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+const closeNav = document.getElementById("closeNav")
+const openNav = document.getElementById("openNav")
+const openNav2 = document.getElementById("openNav2")
+const responsiveNavbar = document.getElementById("responsiveNavbar")
+
+openNav.addEventListener('click', () => {
+    responsiveNavbar.style.width = "20rem"
+    responsiveNavbar.classList.add("border-l")
+})
+closeNav.addEventListener('click', () => {
+    responsiveNavbar.style.width = "0rem"
+    responsiveNavbar.classList.remove("border-l")
+})
+openNav2.addEventListener('click', () => {
+    responsiveNavbar.style.width = "20rem"
+    responsiveNavbar.classList.add("border-l")
+})
